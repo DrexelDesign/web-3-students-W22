ad1bc384b3965304ca0dc5edc5fb3acf55bd742b
Josh <joshGdovindesigns.com>
2022-01-13 14:27:48 -0500

    updated demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,10 +1,59 @@
-import { skillsTrigger } from "./scrollAnimation.js"
+// import { skillsTrigger } from "./scrollAnimation.js"
+
+import { slideInRight } from "./scrollAnimation.js"
+
+
+
+// var
+// const
+// final
+
+
 
 window.addEventListener('load', function(){
-    // this will call the skillsTrigger function located in the scrollAnimation.js file
-    skillsTrigger("right center", ".gallery","#skynet");
 
-    skillsTrigger("left center",".yellow-bar","#skills-content");
+
+
+    // let number = 10;
+    // let anotherNumber = 3;
+
+
+    // number = "ten";
+
+    // console.log(number);
+
+
+    // let answer = number + anotherNumber;
+    // console.log(answer);
+
+    // index values               0                   1                   2
+    let triggerElements = ["#history-middle","#history-first", "#skills-content"];
+    // console.log(triggerElements);
+    // console.log(triggerElements.length);
+    //console.log(triggerElements[0]);
+
+    // index values                  0                   1                   2
+    let animationElements =["#history-middle p","#robot-profile", "#skills-content h1"];
+    // console.log(animationElements);
+    // console.log(animationElements.length)
+
+        //  i = i + 1;   === i++
+    for(let i = 0; i < triggerElements.length; i++){
+        console.log(triggerElements[i]);
+        slideInRight(triggerElements[i], animationElements[i]);
+    }
+
+
+    // slideInRight();
+
+    // slideInRight(triggerElements[0], animationElements[0]);
+
+    // slideInRight("#history-first", "#robot-profile");
+
+    // this will call the skillsTrigger function located in the scrollAnimation.js file
+    // skillsTrigger("right center", ".gallery","#skynet");
+
+    // skillsTrigger("left center",".yellow-bar","#skills-content");
 });
 
 
--- dev/js/scrollAnimation.js
+++ dev/js/scrollAnimation.js
@@ -3,27 +3,45 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+
+export function slideInRight(triggerElement,animationElement ){
+    const tl = new gsap.timeline();
+    tl.from(animationElement,{
+        duration:1,
+        x: -500,
+        alpha:0,
+        scrollTrigger:{
+            trigger: triggerElement,
+            scrub:true,
+            toggleActions: "restart none resume none",
+            markers: true
+        }
+    });
+}
+
+
 // gsap.set(".gallery",{transformOrigin:"left center"});
 
 
-export function skillsTrigger(originDirection, animationElement, triggerElement){
-    const tl = new gsap.timeline();
+// export function skillsTrigger(originDirection, animationElement, triggerElement){
+//     const tl = new gsap.timeline();
 
-    tl.from(animationElement,{
-        // duration:3, 
-        scaleX:0,
-        transformOrigin: originDirection,
-        scrollTrigger: {
-            trigger: triggerElement,
-            start:"top 60%",
-            end: "bottom 80%",
-            toggleActions: "restart none resume none",
-            scrub: true,
-            markers: true
-        },
-        stagger:0.25
-    });
-}
+//     tl.from(animationElement,{
+//         // duration:3, 
+//         scaleX:0,
+//         transformOrigin: originDirection,
+//         scrollTrigger: {
+//             trigger: animationElement,
+//             start:"top 60%",
+//             end: "bottom 80%",
+//             toggleActions: "restart none resume none",
+//             scrub: 2,
+//             pin: "#skills",
+//             markers: true
+//         },
+//         stagger:0.25
+//     });
+// }
 
 
 
