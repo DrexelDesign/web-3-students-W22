d236c1b98fd5c81dd02a77bf41d62cf5121b7e31
Josh <joshGdovindesigns.com>
2022-01-18 14:20:18 -0500

    updated mixin

--- dev/js/scrollAnimation.js
+++ dev/js/scrollAnimation.js
@@ -1,8 +1,6 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 gsap.registerPlugin(ScrollTrigger);
-
 
 export function slideInRight(triggerElement,animationElement ){
     const tl = new gsap.timeline();
@@ -12,13 +10,12 @@ export function slideInRight(triggerElement,animationElement ){
         alpha:0,
         scrollTrigger:{
             trigger: triggerElement,
-            scrub:true,
             toggleActions: "restart none resume none",
-            markers: true
+            // markers: true
+            scrub: true
         }
     });
 }
-
 
 export function slideInLeft(triggerElement,animationElement ){
     const tl = new gsap.timeline();
@@ -28,43 +25,9 @@ export function slideInLeft(triggerElement,animationElement ){
         alpha:0,
         scrollTrigger:{
             trigger: triggerElement,
-            scrub:true,
             toggleActions: "restart none resume none",
-            markers: true
+            // markers: true
+            scrub: true
         }
     });
 }
-
-
-// gsap.set(".gallery",{transformOrigin:"left center"});
-
-
-// export function skillsTrigger(originDirection, animationElement, triggerElement){
-//     const tl = new gsap.timeline();
-
-//     tl.from(animationElement,{
-//         // duration:3, 
-//         scaleX:0,
-//         transformOrigin: originDirection,
-//         scrollTrigger: {
-//             trigger: animationElement,
-//             start:"top 60%",
-//             end: "bottom 80%",
-//             toggleActions: "restart none resume none",
-//             scrub: 2,
-//             pin: "#skills",
-//             markers: true
-//         },
-//         stagger:0.25
-//     });
-// }
-
-
-
-
-
-
-// export function demoThing(){
-    // console.log("this is working here too!");
-    // console.log("working");
-// }
