858888d5b36668c9319af057b978e507096e712d
Josh <joshGdovindesigns.com>
2022-01-18 15:21:13 -0500

    update demo

--- dev/js/burgerAnimation.js
+++ dev/js/burgerAnimation.js
@@ -3,4 +3,10 @@ import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 gsap.registerPlugin(DrawSVGPlugin);
 
 
+
+
+
 export const burgerTL = new gsap.timeline({paused:true});
+
+
+burgerTL.to("#outline",{duration:1, alpha:0});
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,9 +1,20 @@
-import { slideInRight, slideInLeft} from "./scrollAnimation.js"
+// import { slideInRight, slideInLeft} from "./scrollAnimation.js";?
+import { burgerTL } from "./burgerAnimation";
+
+var burgerButton = document.querySelector("#burger");
+
+burgerButton.addEventListener("click", () =>{
+    console.log("burger clicked");
+});
+
+
+
+
 
 
 window.addEventListener('load', function(){
 
-    // index values               0                   1                   2
+    // // index values               0                   1                   2
     let triggerElements = ["#history-middle","#history-first", "#skills-content"];
     // index values                  0                   1                   2
     let animationElements =["#history-middle p","#robot-profile", "#skills-content h1"];
@@ -11,10 +22,10 @@ window.addEventListener('load', function(){
 
     for( const element of triggerElements){
         // console.log(buttonIndex);
-        console.log(element + " is an element");
+        // console.log(element + " is an element");
         
         if(element === "#history-first"){
-            console.log("The index is " + buttonIndex);
+            // console.log("The index is " + buttonIndex);
         }
         buttonIndex ++;
     }
