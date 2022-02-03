a43056e3575b7efa513083136fe3cfc62eac139a
Josh <joshGdovindesigns.com>
2022-02-03 12:23:56 -0500

    demo complete

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,19 +1,6 @@
-// import {skillsTrigger,galleryAni} from "./scrollAnimation.js";
-// import {burgerTL} from "./burgerAnimation.js";
-// import {menuAnimation} from "./mobileMenu.js"
-// import {scrollPage} from "./pageScroll"
-// import { displayWindowSize } from "./mobileResizing"
-
 import { setupMenu } from "./slideMenu"
-
 
 window.addEventListener('load', function () {
     setupMenu();
-    // galleryAni();
 })
 
-//listen to window resize
-// window.addEventListener("resize", displayWindowSize);
-
-// //check the window size on load
-// window.addEventListener('load', displayWindowSize); 
--- dev/js/slideMenu.js
+++ dev/js/slideMenu.js
@@ -2,35 +2,13 @@ import { gsap } from "gsap";
 
 gsap.set("nav",{right:0});
 
-
-// gsap.set("#button-1 .nav-rightSide", {scaleX:0, transformOrigin:("left, center")});
-
-// let buttonOne = document.querySelector("#button-1");
-
 let rightSide = document.querySelectorAll(".nav-container .nav-rightSide");
-
 let buttons = document.querySelectorAll(".nav-container aside");
-
-// rightSide.classList.add("active");
-
-// console.log(rightSide);
-
 let currentIndex =  -1;
 
-// let isActive = false;
-
-
-
 export function setupMenu(){
-
-    
-
     buttons.forEach((button , i) => {
-
-
         button.addEventListener("click", () => {
-
-            
 
             console.log(currentIndex + " this is current index");
 
@@ -41,81 +19,17 @@ export function setupMenu(){
 
             currentIndex = i;
             rightSide[i].classList.add("active");
-
         })
-
-
-
-        // if(button.classList.contains("active")){
-        //     // button.classList.remove("active");
-        // }else{
-        //     button.classList.add("active");
-        // }
     })
 
-    
-    // buttonOne.addEventListener("click", ()=>{
 
-    //     if(!isActive){
-    //         rightSide.classList.add("active");
-    //         isActive = true;
-    //     }else{
-    //         rightSide.classList.remove("active");
-    //         isActive = false;
-    //     }
+    // add a listener to the content section
+    let content = document.querySelector("#content");
 
-    // });
+    content.addEventListener("click", () => {
+        rightSide[currentIndex].classList.remove("active");
 
-
-    // gsap.set("#button-1 .nav-rightSide", {scaleX:1, transformOrigin:("right, center")});
-
-
-    // let navContainer = document.querySelectorAll(".nav-container");
-    // let navRightSide = document.querySelectorAll(".nav-rightSide");
-    // let navButton = document.querySelectorAll(".nav-content button");
-
-    // // gsap.to("#button-1",{x:"-=500"});
-
-    // // let numbersToMove = [50,100,150];
-
-    // navRightSide.forEach((button,i) => {
-
-    //     console.log(button);
-
-    //     if(i == 1){
-    //         button.style.flexGrow = 4;
-    //     }
-        
-
-    //     // button.addEventListener("click", e =>{
-    //     //     if(i){
-    //     //         console.log("YES");
-    //     //         // gsap.to(navContainer[i],{flexGrow:4});
-    //     //     }else{
-    //     //         // gsap.to(navContainer[i],{flexGrow:1});
-    //     //         console.log("NO");
-    //     //     }
-    //     // })
-
-        
-    // });
+        currentIndex = -1;
+    })
 
 }
-
-
-    // for(let i = 0; i< navContainer.length; i++){
-
-    //     navButton[i].addEventListener("click", ()=>{
-    //         //gsap.to(navContainer[i],{x:"-=" + (navRightSide[i].offsetWidth - numbersToMove[i]) });
-    //         gsap.to(navContainer[i],{width:"+=85vw"});
-    //         if(i == ){
-
-    //         }
-    //     })
-        
-    //     // let numberConvert = (navRightSide[i].offsetWidth + numbersToMove[i]).toString();
-    //     // console.log(numberConvert);
-    //     // console.log(navRightSide[i].offsetWidth);
-    //     // gsap.set(navContainer[i],{x:"+=" + numberConvert });
-    // }
-
