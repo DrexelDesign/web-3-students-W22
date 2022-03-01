8d6d978dd13f49dfd7003a85eed7b1372d79384d
Josh <joshGdovindesigns.com>
2022-02-28 23:10:32 -0500

    working on demo

--- dev/js/projects.js
+++ dev/js/projects.js
@@ -1,11 +1,17 @@
 export let projects = [
     {
-        title:"Tutrles",
+        title:"Turtles",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sagittis maximus arcu mattis fringilla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam interdum eleifend commodo. Morbi pretium egestas tincidunt. Nunc porta risus vitae nunc facilisis consequat. Integer justo ipsum, lacinia non vestibulum vel, tincidunt ut augue. Nullam elementum nunc a ultricies viverra. Duis malesuada massa eros, a euismod mauris bibendum non. Curabitur tellus erat, posuere ac leo ac, pellentesque consectetur turpis. Mauris in blandit nibh, sit amet condimentum eros. Mauris ultrices orci a ipsum hendrerit efficitur. Nunc tempor nulla dui, nec cursus urna dictum at.",
-        images:['./img/projects/turtles/turtle-1.jpg']
+        images:['./img/projects/turtles/turtle-1.jpg','./img/projects/turtles/turtle-2.jpg']
     },
     {
-        title:"Project 2",
-        description:"Aliquam facilisis vestibulum ullamcorper. Sed nec commodo lorem. Cras bibendum purus et est tristique, eu commodo nibh aliquam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla eu luctus ex. Curabitur est quam, luctus bibendum ante vitae, posuere tincidunt lorem. Morbi condimentum lacinia semper. In sed lorem sit amet velit mattis aliquam et sit amet ex. Praesent et cursus quam, vel vehicula sem. Phasellus sollicitudin pretium magna, id vehicula elit suscipit eu. Quisque dignissim metus orci, in ultrices lacus egestas nec. Proin accumsan quam vel diam luctus, sit amet hendrerit tortor hendrerit. Etiam nisi felis, imperdiet condimentum tincidunt nec, dignissim porta tellus."
+        title:"Cows",
+        description:"Aliquam facilisis vestibulum ullamcorper. Sed nec commodo lorem. Cras bibendum purus et est tristique, eu commodo nibh aliquam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla eu luctus ex. Curabitur est quam, luctus bibendum ante vitae, posuere tincidunt lorem. Morbi condimentum lacinia semper. In sed lorem sit amet velit mattis aliquam et sit amet ex. Praesent et cursus quam, vel vehicula sem. Phasellus sollicitudin pretium magna, id vehicula elit suscipit eu. Quisque dignissim metus orci, in ultrices lacus egestas nec. Proin accumsan quam vel diam luctus, sit amet hendrerit tortor hendrerit. Etiam nisi felis, imperdiet condimentum tincidunt nec, dignissim porta tellus.",
+        images:['./img/projects/cows/cow-1.jpg','./img/projects/cows/cow-2.jpg']
+    },
+    {
+        title:"Zoo Animals",
+        description:"Aliquam facilisis vestibulum ullamcorper. Sed nec commodo lorem. Cras bibendum purus et est tristique, eu commodo nibh aliquam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla eu luctus ex. Curabitur est quam, luctus bibendum ante vitae, posuere tincidunt lorem. Morbi condimentum lacinia semper. In sed lorem sit amet velit mattis aliquam et sit amet ex. Praesent et cursus quam, vel vehicula sem. Phasellus sollicitudin pretium magna, id vehicula elit suscipit eu. Quisque dignissim metus orci, in ultrices lacus egestas nec. Proin accumsan quam vel diam luctus, sit amet hendrerit tortor hendrerit. Etiam nisi felis, imperdiet condimentum tincidunt nec, dignissim porta tellus.",
+        images:['./img/projects/zoo/zoo-1.jpg','./img/projects/zoo/zoo-2.jpg']
     }
 ]; 
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -11,6 +11,16 @@ heroImgArray.forEach((heroImg, i) => {
     heroImg.style.backgroundImage = "url(" + projects[i].images[0] + ")";
 });
 
+let heroLinks = document.querySelectorAll("#gallery a");
+
+heroLinks.forEach((link, i) => {
+    console.log("hello");
+});
+
+
+  // localStorage.setItem("indexValue", $(".hero-btn").index(this));
+    // window.document.location = './detail.html' + '?studentName=' + students[localStorage.getItem("indexValue")].id
+
 
 
 
