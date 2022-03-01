49fdae942f1b268c670b61447f5126152b00008f
Josh <joshGdovindesigns.com>
2022-02-28 23:07:44 -0500

    working on demo

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -5,4 +5,13 @@ console.log(projects[0].title);
 
 
 // populate the hero images
-let heroImgArray = document.querySelectorAll();
+let heroImgArray = document.querySelectorAll('#gallery li');
+// iterate over the heroImgArray and populate the background images
+heroImgArray.forEach((heroImg, i) => {
+    heroImg.style.backgroundImage = "url(" + projects[i].images[0] + ")";
+});
+
+
+
+
+
