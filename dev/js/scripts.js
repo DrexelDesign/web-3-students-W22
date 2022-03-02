609c7f50b126c09758d1e34313ba47eb8a627a63
Josh <joshGdovindesigns.com>
2022-03-01 20:47:37 -0500

    demo complete

---
+++ dev/js/scripts.js
@@ -0,0 +1,10 @@
+// import { multiscroll } from " multiscroll "
+// import $ from "jquery"
+
+$(function(){
+    console.log($);
+    $('#multiscroll').multiscroll();
+});
+// console.log("hello");
+
+
