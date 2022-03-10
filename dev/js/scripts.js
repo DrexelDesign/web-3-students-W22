c451a09bc8fb10f432e1c9d4ab7048d84a04dd3a
Josh <joshGdovindesigns.com>
2022-03-10 09:02:56 -0500

    Revert "demo complete"
    This reverts commit 0b6ba2b46264bd9d611314e2c0c386a6aee9621b.

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,29 +1,9 @@
-import { slideInRight, slideInLeft} from "./scrollAnimation.js"
-window.addEventListener('load', function(){
+import {slideInRight,slideInLeft} from "./scrollAnimation.js"
+
+window.addEventListener('load', function () {
 
     const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
-
-    function switchTheme(e) {
-
-       
-
-        if (e.target.checked) {
-            document.documentElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark'); //add this
-        }
-        else {
-            document.documentElement.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light'); //add this
-        }    
-
-
-        console.log(toggleSwitch.checked + ' this is the value for the checkbox');
-        e.stopPropagation();
-    }
-
-    toggleSwitch.addEventListener('change', switchTheme, false);
-
-    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+    const currentTheme = localStorage.getItem('theme');
 
     if (currentTheme) {
         document.documentElement.setAttribute('data-theme', currentTheme);
@@ -33,42 +13,39 @@ window.addEventListener('load', function(){
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    let triggerElements = ["#history-middle","#history-first", "#skills-content"];
-    let animationElements =["#history-middle p","#robot-profile", "#skills-content h1"];
-    let buttonIndex = 0;
-
-    for( const element of triggerElements){
-        // console.log(buttonIndex);
-        console.log(element + " is an element");
-        
-        if(element === "#history-first"){
-            console.log("The index is " + buttonIndex);
+    function switchTheme(e) {
+        console.log(e.target.checked);
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
         }
-        buttonIndex ++;
     }
 
-    for(let i = 0; i < triggerElements.length; i++){
-        if( i === triggerElements.length/2){
+    toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+
+    let triggerElements = ["#history-middle", "#history-first", "#skills-content"];
+    let animationElements = ["#history-middle p", "#robot-profile", "#skills-content h1"];
+    let buttonIndex = 0;
+
+    for (const element of triggerElements) {
+        // console.log(buttonIndex);
+        console.log(element + " is an element");
+
+        if (element === "#history-first") {
+            console.log("The index is " + buttonIndex);
+        }
+        buttonIndex++;
+    }
+
+    for (let i = 0; i < triggerElements.length; i++) {
+        if (i === triggerElements.length / 2) {
             slideInLeft(triggerElements[i], animationElements[i]);
-        }else{
+        } else {
             slideInRight(triggerElements[i], animationElements[i]);
         }
     }
